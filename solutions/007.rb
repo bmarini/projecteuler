bd57a86c9133e46227b607f8440102a4c64de53e
Ben Marini <bmarini@gmail.com>
2011-01-30 14:45:51 -0800

    Solution 7

---
+++ solutions/007.rb
@@ -0,0 +1,21 @@
+$:.unshift( File.expand_path( File.dirname(__FILE__) ) )
+require 'euler_helpers'
+
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+# that the 6th prime is 13.
+# 
+# What is the 10001st prime number?
+
+primes = Enumerator.new do |yielder|
+  prime = Proc.new { |n| 2.upto( Math.sqrt(n).ceil ).all? { |p| !n.multiple?(p) } }
+
+  yielder.yield(2)
+
+  n = 3
+  loop do
+    yielder.yield(n) if prime.call(n)
+    n += 1
+  end
+end
+
+p primes.take(10001).last
